a432407c60dde7827e957e2aa1770e94a42a3e8d
DragonFruit8 <Jtbyers19@gmail.com>
2025-09-13 13:22:41 -0400

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,19 @@
-// import DonationPool from "../components/DonationPool.jsx";
+import DonationPool from "../components/DonationPool.jsx";
 import HeroSection from "../components/HeroSection.jsx";
-// import RecordsDataComponent from "../components/RecordsDataComponent.jsx";
-// import SponsorSection from "../components/SponsorSection.jsx";
-// import UpcomingFamilies from "../components/UpcomingFamilies.jsx";
-import FamilyForm from '../forms/FamilyForm.jsx'
+import RecordsDataComponent from "../components/RecordsDataComponent.jsx";
+import SponsorSection from "../components/SponsorSection.jsx";
+import UpcomingFamilies from "../components/UpcomingFamilies.jsx";
+// import FamilyForm from '../forms/FamilyForm.jsx'
 const HomePage = () => {
   return (
     <>
       <div className="flex h-100">
         <HeroSection />
-        {/* <RecordsDataComponent />
+        <RecordsDataComponent />
         <DonationPool />
         <UpcomingFamilies />
-        <SponsorSection /> */}
-        <FamilyForm />
+        <SponsorSection />
+        {/* <FamilyForm /> */}
       </div>
     </>
   );
